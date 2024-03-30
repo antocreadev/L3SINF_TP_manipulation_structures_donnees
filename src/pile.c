5d29a44a3061d647f2c8a82178b619830890df89
antocreadev <anthonymenghi@protonmail.com>
2024-03-30 13:46:49 +0100

    add: exo2 fini

--- src/pile.c
+++ src/pile.c
@@ -49,6 +49,39 @@ int depile(Element** p_sommet){
     return data;
 }
 
+void inverse_pile(Element** p_sommet){
+    // on va récupérer la longueur de la pile, on créer un pointeur temporaire pour le modifier car on aura de sa valeur et de le modifier pour depiler toute la pile
+    Element *p_tempo = * p_sommet;
+    int taille = 0;
+     while (p_tempo != NULL) {
+        taille++;
+        p_tempo = p_tempo->next;
+    }
+    printf("taille de la pile : %d\n", taille);
+
+    // on créer un tableau pour stcoker les datas de la depilation
+    int* datas = malloc(sizeof(int)* taille); 
+    if (datas == NULL) {
+        printf("Erreur : impossible d'allouer de la mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // on stocke toutes les valeurs de la pile
+    int i=0;
+    for (i = 0; i < taille; i++)
+    {
+    datas[i] = (*p_sommet)->data;
+    *p_sommet = (*p_sommet)->next;
+    }
+
+    // on créer une nouvelle pille en empilant toutes les valeurs de l'ancienne pille 
+    int k = 0;
+    *p_sommet = NULL;
+    for (k = 0; k < taille; k++)
+    {
+        empiler(p_sommet, datas[k]);
+    }
+}
 
 
 int main() {
@@ -68,6 +101,14 @@ int main() {
     empiler(&sommet, 15);
     afficher(sommet);
     depile(&sommet);
+    empiler(&sommet, 15);
     afficher(sommet);
+    printf("--------\n");
+    inverse_pile(&sommet);
+    printf("--------\n");
+    afficher(sommet);
+
+
+
     return 0;
 }
